c9eb3d0774bb61a12cd0f16b1e6ab67aabd22ae7
Siigmaf <faby.tbe@gmail.com>
2023-03-14 13:38:50 -0300

    implementa botão darkmode

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 'use strict'
-
-const btnTheme = document.querySelector('.btn');
+const btnTheme = document.querySelector('.btn')
 
 btnTheme.addEventListener('click', function() {
-    //usando método toggle para alternar o elemento para a classe dark-theme.
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
-    if(className == "ligth-theme") {
-     
-        this.textContent = "Dark";
-    }
-    else {
-        
-        this.textContent = "Ligth";
+    if(className === "dark-theme") {
+      document.body.classList.toggle('light-theme')
     }
     
 });
