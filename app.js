3e05aa09c4a4053d5d41ea4943264786a71b6687
Siigmaf <faby.tbe@gmail.com>
2023-03-22 12:03:41 -0300

    add icone ao botão

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 'use strict'
 const btnTheme = document.querySelector('.btn')
+const icone = document.querySelector('.icone')
 
 btnTheme.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
-    var className = document.body.className;
-    if(className === "dark-theme") {
-      document.body.classList.toggle('light-theme')
+    if( icone.src.includes('light.svg') ) {
+      icone.src = 'dark.svg';
+      
+    }else {
+      icone.src = 'light.svg';
     }
     
 });
